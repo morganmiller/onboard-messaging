cd07becb5286f3c378b4eb2b118cca9539fa114e
Morgan Miller <m.miller722@gmail.com>
2016-11-06 20:26:07 -0700

    selectively hide and show sms messages

--- app/models/message_thread.rb
+++ app/models/message_thread.rb
@@ -1,7 +1,7 @@
 class MessageThread < ActiveRecord::Base
   belongs_to :account
 
-  has_many :sms_messages, -> { order(created_at: :desc) }
+  has_many :sms_messages, -> { order(created_at: :asc) }
 
   scope :sorted, -> { order(created_at: :desc) }
 end
